be3f43fb7ec66e7561fc40a9885586e75d3c8e0b
dmnevius <dmnevius@ncsu.edu>
2021-04-17 14:31:56 -0400

    Integrated webpack-node-externals into backend config

--- config/backend.js
+++ config/backend.js
@@ -1,26 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const repl = require('repl');
 const { merge } = require('webpack-merge');
-const packageJson = require('../package.json');
+const nodeExternals = require('webpack-node-externals');
 const common = require('./common');
-
-const exclude = [];
-const externals = {};
-exclude
-  // eslint-disable-next-line no-underscore-dangle
-  .concat(repl._builtinLibs)
-  .concat(Object.keys(packageJson.dependencies))
-  .forEach((lib) => {
-    externals[lib] = `commonjs ${lib}`;
-  });
 
 module.exports = function backend(input, output) {
   return merge(common(output), {
     entry: {
       backend: input,
     },
-    externals,
+    externals: [nodeExternals()],
     node: false,
+    target: 'node',
   });
 };
