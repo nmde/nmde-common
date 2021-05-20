4ed6a6c9b98f264e82a93379e000ecffee3c7c51
dmnevius <dmnevius@ncsu.edu>
2021-05-20 12:05:21 -0400

    Added missing changes from previous commit

--- config/babel.js
+++ config/babel.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const proposalClassProperties = require('@babel/plugin-proposal-class-properties');
 const proposalDecorators = require('@babel/plugin-proposal-decorators');
+const proposalPrivatePropertyInObject = require('@babel/plugin-proposal-private-property-in-object');
 const babelPresetEnv = require('@babel/preset-env');
 const babelPresetTypescript = require('@babel/preset-typescript');
 const babelPresetJSX = require('@vue/babel-preset-jsx');
@@ -15,6 +16,12 @@ module.exports = () => ({
     ],
     [
       proposalClassProperties,
+      {
+        loose: true,
+      },
+    ],
+    [
+      proposalPrivatePropertyInObject,
       {
         loose: true,
       },
