06967fd03e505824e3e7c1e907165ad118db967f
dmnevius <dmnevius@ncsu.edu>
2021-05-23 12:35:27 -0400

    Changed storybook preview config

--- config/storybook-preview.js
+++ config/storybook-preview.js
@@ -1,12 +1,9 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { VApp } = require('vuetify/dist/vuetify');
-
 module.exports = function preview(vuetify) {
   return {
     decorators: [
       (story) => ({
-        components: { story, VApp },
-        template: '<v-app><v-main><story /></v-main></v-app>',
+        components: { story },
+        template: '<story />',
         vuetify,
       }),
     ],
