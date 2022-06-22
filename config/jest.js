428fcd0f93c143b6dc86da2185a6cf7f8f749e83
Daniel Nevius <dmnevius@ncsu.edu>
2022-06-22 10:37:12 -0600

    Update Jest config

--- config/jest.js
+++ config/jest.js
@@ -1,14 +1,19 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
+  extensionsToTreatAsEsm: ['.ts'],
   globals: {
     'ts-jest': {
       astTransformers: {
         before: ['nmde-common/keys-transformer'],
       },
+      useESM: true,
     },
   },
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '.+\\.jsx?$': 'babel-jest',
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [],
 };
